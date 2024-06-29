f7077fe3e2cf9c0bd02c5b3921cfae9783248b4a
veronicakho94 <kho.veronica@gmail.com>
2024-06-29 18:06:53 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import {
+  SafeAreaView,
+  View,
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+    <View style = {{ justifyContent: 'center', alignItems: 'center'}}>
+     <Text>Hi, I just completed my first React Native Assignment</Text>
+    </View>
+    </SafeAreaView>
+  );
+}
+
+
+export default App;
